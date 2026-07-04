ef5aa04dbd3d38bd470c394638fd26cf72695fba
Ivan Boriev <ivanboriev@gmail.com>
2026-07-04 13:14:58 +0300

    Генерация паролей

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 	"os"
 	"time"
@@ -94,6 +95,29 @@ func (pm *PasswordManager) ListPasswords() []Password {
 
 	return passwords
 
+}
+
+func (pm *PasswordManager) GeneratePassword(length int) (string, error) {
+	if length < 8 {
+		return "", fmt.Errorf("password length must be at least 8 characters")
+	}
+
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?/"
+
+	buffer := make([]byte, length)
+
+	_, err := rand.Read(buffer)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate random password: %v", err)
+	}
+
+	password := make([]byte, length)
+
+	for i, val := range buffer {
+		password[i] = charset[int(val)%len(charset)]
+	}
+
+	return string(password), nil
 }
 
 func main() {
