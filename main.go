e2b453cf6c41c025baf9c196f8c3346dc2f87f92
Ivan Boriev <ivanboriev@gmail.com>
2026-07-04 15:21:50 +0300

    Проверка надёжности

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -215,6 +216,34 @@ func (pm *PasswordManager) LoadFromFile() error {
 	err = json.Unmarshal(decryptedData, &pm.passwords)
 	if err != nil {
 		return fmt.Errorf("failed to unmarshal passwords: %v", err)
+	}
+
+	return nil
+}
+
+func (pm *PasswordManager) CheckPasswordStrength(password string) error {
+
+	if len(password) < 8 {
+		return fmt.Errorf("password must be at least 8 characters long")
+	}
+
+	var hasUpper, hasLower, hasDigit, hasSpecial bool
+
+	for _, char := range password {
+		switch {
+		case 'A' <= char && char <= 'Z':
+			hasUpper = true
+		case 'a' <= char && char <= 'z':
+			hasLower = true
+		case '0' <= char && char <= '9':
+			hasDigit = true
+		case strings.ContainsRune("!@#$%^&*()-_=+[]{}|;:,.<>?/", char):
+			hasSpecial = true
+		}
+	}
+
+	if !hasUpper || !hasLower || !hasDigit || !hasSpecial {
+		return fmt.Errorf("password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
 	}
 
 	return nil
