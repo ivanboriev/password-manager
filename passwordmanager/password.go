833967ca5408d1b143639b8de1805e34b21cdde6
Ivan Boriev <ivanboriev@gmail.com>
2026-07-07 19:12:30 +0300

    upd password struct

--- passwordmanager/password.go
+++ passwordmanager/password.go
@@ -6,8 +6,8 @@ type Password struct {
 	Name         string    `json:"name"`
 	Value        string    `json:"value"`
 	Category     string    `json:"category"`
-	CreatedAt    time.Time `json:"createdAt"`
-	LastModified time.Time `json:"lastModified"`
+	CreatedAt    time.Time `json:"created_at"`
+	LastModified time.Time `json:"last_modified"`
 }
 
 func NewPassword(name, value, category string) Password {
